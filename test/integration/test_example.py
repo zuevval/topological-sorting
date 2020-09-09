76c21108b22041181d6d0b5a7ee3a5905b2e4391
zuevval <valera.zuev.zva@gmail.com>
2020-09-09 11:14:05 +0300

    CI: create minimalistic version of pipeline

--- test/integration/test_example.py
+++ test/integration/test_example.py
@@ -1,8 +1,6 @@
-from test.integration import TestCaseWrapper
+import unittest
 
 
-class TestExample(TestCaseWrapper):
+class TestExample(unittest.TestCase):
     def test_example(self):
         assert True
-        with open(self.OutDir / "example.txt", "w") as file_out:
-            file_out.writelines("Hello Example Output File")
